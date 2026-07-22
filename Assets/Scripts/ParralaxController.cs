ed57c211800ba34f163377e98c0f1335867220cb
Connor K <connor.kian@education.nsw.gov.au>
2026-07-22 16:21:31 +1000

    WIP: parallax
    made infinite parallax scrolling

--- Assets/Scripts/ParralaxController.cs
+++ Assets/Scripts/ParralaxController.cs
@@ -4,6 +4,7 @@ public class ParralaxController : MonoBehaviour
 {
     private float startPosX;
     private float startPosY;
+    private float length;
     public GameObject cam;
     public float parralaxEffect;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -11,6 +12,7 @@ public class ParralaxController : MonoBehaviour
     {
         startPosX=transform.position.x;
         startPosY = transform.position.y;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
@@ -19,7 +21,16 @@ public class ParralaxController : MonoBehaviour
         //vv- 0=move with cam / 1=wont move / 0.5=half 
         float distX = cam.transform.position.x * parralaxEffect;
         float distY = cam.transform.position.y * parralaxEffect;
+        float movement = cam.transform.position.x * (1 - parralaxEffect);
 
         transform.position = new Vector3(startPosX + distX, startPosY + distY+(2*(1-parralaxEffect)),transform.position.z);
+        if (movement > startPosX + length)
+        {
+            startPosX += length;
+        }
+        else if (movement < startPosX - length) 
+        {
+            startPosX -= length;
+        }
     }
 }
